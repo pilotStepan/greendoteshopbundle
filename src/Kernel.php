45222b3948da9e3440a3a4eab5c9921a0b8f1b38
Stepan Sipal <stepan@greendot.cz>
2024-11-21 15:20:24 +0100

    Update pro maker

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Greendot\EshopBundle;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
