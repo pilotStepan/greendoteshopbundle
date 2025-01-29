51102da66c8b277de22dc572c003d50944df4fc4
Ori <ori.rowan@gmail.com>
2025-01-29 10:53:22 +0100

    ManageClientDiscount.php service

--- src/Enum/DiscountType.php
+++ src/Enum/DiscountType.php
@@ -4,9 +4,8 @@ namespace Greendot\EshopBundle\Enum;
 
 enum DiscountType: string
 {
-    case SingleClient = "SINGLE_CLIENT";
-    case SingleUse = "SINGLE_USE";
-    case MultiUse = "MULTI_USE";
-
-    case SingleUseClient = "SINGLE_USE_CLIENT";
+    case SingleClient = "SINGLE_CLIENT"; // pro jednoho clienta, více použití
+    case SingleUse = "SINGLE_USE"; // pro jedno použití
+    case MultiUse = "MULTI_USE"; // pro více použítí
+    case SingleUseClient = "SINGLE_USE_CLIENT"; // pro jenoho clienta, jedno použití
 }
--- src/Service/ManageClientDiscount.php
+++ src/Service/ManageClientDiscount.php
@@ -3,8 +3,8 @@
 namespace Greendot\EshopBundle\Service;
 
 use Greendot\EshopBundle\Entity\Project\ClientDiscount;
-use Greendot\EshopBundle\Entity\Project\Client;
 use Greendot\EshopBundle\Entity\Project\Purchase;
+use Greendot\EshopBundle\Enum\DiscountType;
 
 class ManageClientDiscount
 {
@@ -13,17 +13,20 @@ class ManageClientDiscount
     // checks is_used and date, returns true if ok
     public function isValid(ClientDiscount $clientDiscount) : bool
     {
-        if ($clientDiscount->isIsUsed()){
-            return false;
-        }
-
-        $date = new \DateTime();
-        if (!($clientDiscount->getDateStart() < $date && $date < $clientDiscount->getDateEnd()))
+        if ($clientDiscount->isIsUsed())
         {
             return false;
         }
 
+        $dateStart = $clientDiscount->getDateStart();
+        $dateEnd = $clientDiscount->getDateEnd();
+        $now = new \DateTime();
+
+        if ($dateStart !== null && $dateEnd !== null){
+            return $dateStart <= $now && $now <= $clientDiscount->$dateEnd;
+        }
         return true;
+
     }
 
     // check isValid() and client based on type, returns true if ok
@@ -34,25 +37,24 @@ class ManageClientDiscount
             return false;
         }
 
-        if (($clientDiscount->getType() == "SINGLE_CLIENT" || $clientDiscount->getType() == "SINGLE_USE_CLIENT") && $clientDiscount->getClient() !== $purchase->getClient())
-        {
-           return false;
-        }
+        $isClientSpecific = in_array($clientDiscount->getType(), [DiscountType::SingleClient, DiscountType::SingleUseClient], true);
 
-        return true;
+        return !$isClientSpecific || $clientDiscount->getClient() === $purchase->getClient();
     }
 
     // checks isAvailable and sets is_used=true based on type, returns false if failed, true if ok
     public  function use(ClientDiscount $clientDiscount, Purchase $purchase) : bool
     {
-        if (!$this->isAvailable($clientDiscount, $purchase)){
+        if (!$this->isAvailable($clientDiscount, $purchase))
+        {
             return false;
         }
 
-        if ($clientDiscount->getType() == "SINGLE_USE" || $clientDiscount->getType() == "SINGLE_USE_CLIENT")
+        if (in_array($clientDiscount->getType(), [DiscountType::SingleUse, DiscountType::SingleUseClient], true))
         {
             $clientDiscount->setIsUsed(true);
         }
+
         return true;
     }
 
