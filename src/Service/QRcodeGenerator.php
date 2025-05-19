425c2337b80e2e12b9dceb6c350b5486f193ed17
Ori <ori.rowan@gmail.com>
2025-05-19 13:31:17 +0200

    QR code logo

--- src/Service/QRcodeGenerator.php
+++ src/Service/QRcodeGenerator.php
@@ -21,23 +21,32 @@ class QRcodeGenerator
 
     public function getUri(Purchase $purchase, \DateTimeInterface $dueDate): string
     {
+
         $qrContent = 'SPD*1.0*ACC:CZ1020100000002802559702*AM:' .
             number_format($purchase->getTotalPrice(), 2, '.', '') .
             '*CC:CZK*DT:' . $dueDate->format("Y.m.d") .
             '*X-VS:' . $purchase->getInvoiceNumber();
 
-        $result = new Builder(
-            writer: new PngWriter(),
-            writerOptions: [],
-            validateResult: false,
-            data: $qrContent,
-            encoding: new Encoding('UTF-8'),
-            errorCorrectionLevel: ErrorCorrectionLevel::High,
-            size: 300,
-            margin: 10,
-            roundBlockSizeMode: RoundBlockSizeMode::Margin
-        );
-        $result = $result->build();
+        $builderParams = [
+            'writer' => new PngWriter(),
+            'writerOptions' => [],
+            'validateResult' => false,
+            'data' => $qrContent,
+            'encoding' => new Encoding('UTF-8'),
+            'errorCorrectionLevel' => ErrorCorrectionLevel::High,
+            'size' => 300,
+            'margin' => 10,
+            'roundBlockSizeMode' => RoundBlockSizeMode::Margin,
+        ];
+
+        $logoPath = 'build/img/logo_qr.jpg'; // Must be an absolute or relative server path
+        if (file_exists($logoPath)) {
+            $builderParams['logoPath'] = $logoPath;
+            $builderParams['logoResizeToWidth'] = 100;
+        }
+
+        $builder = new Builder(...$builderParams);
+        $result = $builder->build();
 
         $filePath = sprintf('QRcodes/qr_code_%s.png', $purchase->getId());
         $fullPath = 'public/' . $filePath;
