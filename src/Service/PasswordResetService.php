66994e55e45d58f7613d92dfeb00a667c39372a3
artyom <plokhikhartyom10@gmail.com>
2025-06-17 17:21:08 +0200

    password reset errors handler

--- src/Service/PasswordResetService.php
+++ src/Service/PasswordResetService.php
@@ -6,6 +6,7 @@ use Greendot\EshopBundle\Repository\Project\ClientRepository;
 use RuntimeException;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use SymfonyCasts\Bundle\ResetPassword\Exception\ResetPasswordExceptionInterface;
+use SymfonyCasts\Bundle\ResetPassword\Exception\TooManyPasswordRequestsException;
 use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordToken;
 use SymfonyCasts\Bundle\ResetPassword\ResetPasswordHelperInterface;
 
@@ -34,12 +35,18 @@ readonly class PasswordResetService
     public function processSendingPasswordResetEmail(string $emailFormData): ResetPasswordToken
     {
         if (!$user = $this->clientRepository->findNonAnonymousByEmail($emailFormData)) {
-            throw new RuntimeException('Uživatel s tímto e-mailem nebyl nalezen');
+            throw new RuntimeException('Registrovaný uživatel s tímto e-mailem nebyl nalezen');
         }
 
         try {
             $resetToken = $this->resetPasswordHelper->generateResetToken($user);
             $this->manageMails->sendPasswordResetEmail($user->getMail(), $resetToken);
+        } catch (TooManyPasswordRequestsException $e) {
+            $availableAt = $e->getAvailableAt();
+            $now = new \DateTimeImmutable('now');
+            $remainingSeconds = $availableAt->getTimestamp() - $now->getTimestamp();
+            $remainingMinutes = max(1, ceil($remainingSeconds / 60));
+            throw new RuntimeException(sprintf('Příliš mnoho pokusů o reset hesla. Zkuste to znovu za %d minut', $remainingMinutes));
         } catch (ResetPasswordExceptionInterface|TransportExceptionInterface $e) {
             throw new RuntimeException('Nepodařilo se resetovat heslo, zkuste to prosím znovu později');
         }
