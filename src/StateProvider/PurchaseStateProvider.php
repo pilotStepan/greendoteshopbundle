9df6d86ef5478404e5df90ace89ce79f8cf564c8
Ori <ori.rowan@gmail.com>
2025-08-15 11:02:48 +0200

    No purchase in session exception

--- src/StateProvider/PurchaseStateProvider.php
+++ src/StateProvider/PurchaseStateProvider.php
@@ -7,6 +7,8 @@ use ApiPlatform\State\ProviderInterface;
 use Greendot\EshopBundle\Service\ManagePurchase;
 use Greendot\EshopBundle\Entity\Project\Purchase;
 use Greendot\EshopBundle\Repository\Project\PurchaseRepository;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\Response;
 
 readonly class PurchaseStateProvider implements ProviderInterface
 {
@@ -18,7 +20,7 @@ readonly class PurchaseStateProvider implements ProviderInterface
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): ?Purchase
     {
         $purchase = $this->purchaseRepository->findOneBySession('purchase');
-        if (!$purchase) return null;
+        if (!$purchase) throw new HttpException(Response::HTTP_NO_CONTENT, 'No purchase in session');;
 
         $this->managePurchase->preparePrices($purchase);
 
