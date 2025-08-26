6dde6e722e4195240055693a965678730ccc8c89
Matyas <pribyma@gmail.com>
2025-08-26 09:27:12 +0200

    master controller, translatable routing

--- src/Controller/MasterController.php
+++ src/Controller/MasterController.php
@@ -5,6 +5,7 @@ namespace Greendot\EshopBundle\Controller;
 use Greendot\EshopBundle\Entity\Project\Category;
 use Greendot\EshopBundle\Repository\Project\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,11 +14,16 @@ class MasterController extends AbstractController
 {
 
     #[Route('/{slug}', name: 'app_master', options: ['expose' => true], priority: 1)]
-    public function index(string $slug, CategoryRepository $categoryRepository): Response
+    public function index(string $slug, CategoryRepository $categoryRepository, ParameterBagInterface $parameterBag): Response
     {
+        $availableLocales = $parameterBag->get('app.available.locales');
+        if ($availableLocales and in_array($slug, $availableLocales)){
+            return $this->forward('Greendot\EshopBundle\Controller\Web\HomepageController::index', ['_locale' => $slug]);
+        }
+
         $category = $categoryRepository->findOneBy(['slug' => $slug]);
         if (!$category){
-            $this->createNotFoundException('Category not found');
+            return $this->createNotFoundException('Category not found');
         }
 
         if ($category->getCategoryType() && $category->getCategoryType()->getControllerName()) {
