59e7ac51e1c2e1b1345074c27b81a76ef8c68fe6
Matyas <pribyma@gmail.com>
2025-08-29 12:07:15 +0200

    Added missing tables,fields and relations ships in entities that were used on Optixs. Added some twig functions

--- src/Enum/DownloadRestriction.php
+++ src/Enum/DownloadRestriction.php
@@ -4,6 +4,7 @@ namespace Greendot\EshopBundle\Enum;
 
 enum DownloadRestriction: int
 {
+    case Default = 0;
     case NoRestrictions = 1;
     case EmailRequired = 2;
     case ConfirmationRequired = 3;
