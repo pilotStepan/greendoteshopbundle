c9dd823e721b96034c7ef4ebd008e91333fb5da6
artyom <plokhikhartyom10@gmail.com>
2025-09-09 12:14:29 +0200

    async chat message tweaks

--- src/Message/Notification/PurchaseDiscussionEmail.php
+++ src/Message/Notification/PurchaseDiscussionEmail.php
@@ -2,13 +2,10 @@
 
 namespace Greendot\EshopBundle\Message\Notification;
 
-use DateTimeImmutable;
-
 class PurchaseDiscussionEmail
 {
     public function __construct(
-        public int               $purchaseId,
-        public string            $content,
-        public DateTimeImmutable $createdAt,
+        public int    $purchaseId,
+        public string $content,
     ) {}
 }
