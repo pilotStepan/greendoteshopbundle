200c83743e6f0aa80ef6972aac897da201c53215
artyom <plokhikhartyom10@gmail.com>
2025-09-11 10:42:44 +0200

    state providers

--- src/StateProvider/PurchaseWishlistStateProvider.php
+++ src/StateProvider/PurchaseWishlistStateProvider.php
@@ -5,36 +5,36 @@ namespace Greendot\EshopBundle\StateProvider;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Workflow\Registry;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Response;
 use Greendot\EshopBundle\Entity\Project\Client;
 use Greendot\EshopBundle\Entity\Project\Purchase;
-use Greendot\EshopBundle\Repository\Project\PurchaseRepository;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\Workflow\Registry;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Greendot\EshopBundle\Repository\Project\PurchaseRepository;
 
 readonly class PurchaseWishlistStateProvider implements ProviderInterface
 {
-
     public function __construct(
         private EntityManagerInterface $em,
         private PurchaseRepository     $purchaseRepository,
         private Registry               $workflowRegistry,
         private Security               $security,
         private RequestStack           $requestStack,
-    )
-    {
-    }
+    ) {}
 
-    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): Purchase
     {
-        /* @var Client $user */
-        // Ensure user is present
-        if (!$user = $this->security->getUser()) return null;
+        $client = $this->security->getUser();
+        if (!$client instanceof Client) {
+            throw new HttpException(Response::HTTP_NO_CONTENT);
+        }
 
         // Check if wishlist already exists in session or for the client, if not, create a new one
         $wishlist = $this->purchaseRepository->findWishlistBySession()
-            ?: $this->purchaseRepository->findWishlistForClient($user)
-                ?: $this->createWishlist($user);
+            ?: $this->purchaseRepository->findWishlistForClient($client)
+                ?: $this->createWishlist($client);
 
         // Store wishlist ID in session
         $this->requestStack->getSession()->set('wishlist', $wishlist->getId());
@@ -42,12 +42,13 @@ readonly class PurchaseWishlistStateProvider implements ProviderInterface
         return $wishlist;
     }
 
-    private function createWishlist(Client $user): Purchase
+    private function createWishlist(Client $client): Purchase
     {
         $wishlist = (new Purchase())
             ->setDateIssue(new \DateTime())
             ->setState('draft')
-            ->setClient($user);
+            ->setClient($client)
+        ;
 
         $this->em->persist($wishlist);
         $this->em->flush();
