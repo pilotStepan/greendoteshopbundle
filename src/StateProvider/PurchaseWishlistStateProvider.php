cf4b2e57d825c2118e0f777399e41f9ff0de7036
artyom <plokhikhartyom10@gmail.com>
2025-09-30 14:26:01 +0200

    wishlist token handling

--- src/StateProvider/PurchaseWishlistStateProvider.php
+++ src/StateProvider/PurchaseWishlistStateProvider.php
@@ -10,27 +10,20 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Greendot\EshopBundle\Entity\Project\Client;
 use Greendot\EshopBundle\Entity\Project\Purchase;
-use Greendot\EshopBundle\Enum\VatCalculationType;
+use Greendot\EshopBundle\Service\WishlistService;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Greendot\EshopBundle\Service\CurrencyResolver;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Greendot\EshopBundle\Service\Price\PurchasePriceFactory;
 use Greendot\EshopBundle\Repository\Project\PurchaseRepository;
-use Greendot\EshopBundle\Repository\Project\CurrencyRepository;
-use Greendot\EshopBundle\Service\Price\ProductVariantPriceFactory;
 
 readonly class PurchaseWishlistStateProvider implements ProviderInterface
 {
     public function __construct(
-        private EntityManagerInterface     $em,
-        private PurchaseRepository         $purchaseRepository,
-        private Registry                   $workflowRegistry,
-        private Security                   $security,
-        private RequestStack               $requestStack,
-        private PurchasePriceFactory       $purchasePriceFactory,
-        private ProductVariantPriceFactory $productVariantPriceFactory,
-        private CurrencyResolver           $currencyResolver,
-        private CurrencyRepository         $currencyRepository,
+        private EntityManagerInterface $em,
+        private PurchaseRepository     $purchaseRepository,
+        private Registry               $workflowRegistry,
+        private Security               $security,
+        private RequestStack           $requestStack,
+        private WishlistService        $wishlistService,
     ) {}
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): Purchase
@@ -45,7 +38,7 @@ readonly class PurchaseWishlistStateProvider implements ProviderInterface
             ?: $this->purchaseRepository->findWishlistForClient($client)
                 ?: $this->createWishlist($client);
 
-        $this->preparePrices($wishlist);
+        $this->wishlistService->preparePrices($wishlist);
 
         // Store wishlist ID in session
         $this->requestStack->getSession()->set('wishlist', $wishlist->getId());
@@ -72,42 +65,5 @@ readonly class PurchaseWishlistStateProvider implements ProviderInterface
         }
 
         return $wishlist;
-    }
-
-    private function preparePrices(Purchase $wishlist): void
-    {
-        $main = $this->currencyRepository->findOneBy(['isDefault' => 1]);
-        $secondary = $this->currencyRepository->findOneBy(['name' => 'Euro']);
-
-        $priceCalc = $this->purchasePriceFactory->create($wishlist, $main, VatCalculationType::WithVAT);
-        $totalWithVatMain = $priceCalc->getPrice();
-
-        $priceCalc->setVatCalculationType(VatCalculationType::WithoutVAT);
-        $totalNoVatMain = $priceCalc->getPrice();
-
-        $priceCalc->setCurrency($secondary);
-        $totalNoVatSecondary = $priceCalc->getPrice();
-
-        $priceCalc->setVatCalculationType(VatCalculationType::WithVAT);
-        $totalWithVatSecondary = $priceCalc->getPrice();
-
-        $wishlist->setPrices([
-            'total_with_vat_main' => $totalWithVatMain,
-            'total_no_vat_main' => $totalNoVatMain,
-            'total_with_vat_secondary' => $totalWithVatSecondary,
-            'total_no_vat_secondary' => $totalNoVatSecondary,
-        ]);
-
-        $resolvedCurrency = $this->currencyResolver->resolve();
-        foreach ($wishlist->getProductVariants() as $productVariant) {
-            $productVariantPriceCalc = $this->productVariantPriceFactory->create(
-                $productVariant,
-                $resolvedCurrency,
-                vatCalculationType: VatCalculationType::WithVAT,
-            );
-            $productVariant->setTotalPrice(
-                $productVariantPriceCalc->getPrice(),
-            );
-        }
     }
 }
