cb3d41f35b5ffea4534c4a4114ed2f5b711aa016
Matyas <pribyma@gmail.com>
2025-11-18 10:23:40 +0100

    distinguish color by parameterGroupFormat instead of filter Type

--- src/Entity/Project/ParameterGroupFormat.php
+++ src/Entity/Project/ParameterGroupFormat.php
@@ -7,6 +7,7 @@ use Greendot\EshopBundle\Repository\Project\ParameterGroupFormatRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ParameterGroupFormatRepository::class)]
 class ParameterGroupFormat
@@ -14,9 +15,11 @@ class ParameterGroupFormat
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['parameter_filtered:read', 'parameter:read', 'parameter_group:read', 'category_parameter_group:read', 'product_variant:read', 'product_item:read', 'product_list:read', 'comment:read', 'purchase:read', 'purchase:wishlist'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['parameter_filtered:read', 'parameter:read', 'parameter_group:read', 'category_parameter_group:read', 'product_variant:read', 'product_item:read', 'product_list:read', 'comment:read', 'purchase:read', 'purchase:wishlist'])]
     private ?string $name = null;
 
     #[ORM\OneToMany(mappedBy: 'parameterGroupFormat', targetEntity: ParameterGroup::class)]
