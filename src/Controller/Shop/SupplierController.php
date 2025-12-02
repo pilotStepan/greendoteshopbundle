1ebe88adc28c8c99e136ba6032e75525e42437b9
artyom <plokhikhartyom10@gmail.com>
2025-12-02 13:24:08 +0100

    merge local

--- src/Controller/Shop/SupplierController.php
+++ src/Controller/Shop/SupplierController.php
@@ -9,16 +9,9 @@ use Greendot\EshopBundle\Repository\Project\ProducerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SupplierController extends AbstractController
 {
-    public function __construct(
-        private readonly TranslatorInterface $translator
-    )
-    {
-    }
     #[TranslatableRoute(class: Category::class, property: 'slug')]
     #[Route('/{slug}', name: 'shop_supplier_list')]
     public function producerList(
@@ -45,12 +38,8 @@ class SupplierController extends AbstractController
         requirements: ['slug' => '[a-z0-9\-]+'],
         priority: 2)
     ]
-    public function producerProducts(
-        Producer $producer
-    ): Response
+    public function producerProducts(Producer $producer): Response
     {
-
-
         return $this->render('shop/supplier/products.html.twig', [
             'title'    => $producer->getName(),
             'supplier' => $producer
