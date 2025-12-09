fa0391ee48489d6b9095017d853adbbe831d6c05
artyom <plokhikhartyom10@gmail.com>
2025-12-09 15:44:57 +0100

    master controller simplified

--- src/Controller/MasterController.php
+++ src/Controller/MasterController.php
@@ -3,26 +3,18 @@
 namespace Greendot\EshopBundle\Controller;
 
 use Greendot\EshopBundle\Attribute\TranslatableRoute;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Greendot\EshopBundle\Entity\Project\Category;
 use Greendot\EshopBundle\Enum\ReservedCategoryIds;
-use Greendot\EshopBundle\Repository\Project\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MasterController extends AbstractController
 {
-
     #[TranslatableRoute(class: Category::class, property: 'slug')]
     #[Route('/{slug}', name: 'app_master', options: ['expose' => true], priority: 1)]
-    public function index(string $slug, CategoryRepository $categoryRepository, ParameterBagInterface $parameterBag): Response
+    public function index(Category $category): Response
     {
-        $category = $categoryRepository->findOneByHinted(['slug' => $slug]);
-        if (!$category) {
-            return $this->createNotFoundException('Category not found');
-        }
-        assert($category instanceof Category);
         if ($category->getId() === ReservedCategoryIds::BLOG->value) {
             return $this->forward('Greendot\EshopBundle\Controller\Web\BlogController::blogLandingPage', ['blogSlug' => $category->getSlug()]);
         }
