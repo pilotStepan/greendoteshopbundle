356a8a41b7d7682f1d209c04c82a1980e1e05af3
Ori <ori.rowan@gmail.com>
2026-02-09 16:58:11 +0100

    bug fixes

--- src/Controller/Shop/ProductController.php
+++ src/Controller/Shop/ProductController.php
@@ -28,7 +28,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
---
+++ src/Entity/Project/LogRecord.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Greendot\EshopBundle\Entity\Project;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(
+    name: 'log_record',
+    indexes: [
+        new ORM\Index(name: 'idx_channel_datetime', columns: ['channel', 'datetime']),
+        new ORM\Index(name: 'idx_level_datetime', columns: ['level', 'datetime']),
+    ]
+)]
+final class LogRecord
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'bigint', options: ['unsigned' => true])]
+    private int $id;
+
+    #[ORM\Column(type: 'datetime_immutable', precision: 6)]
+    private \DateTimeImmutable $datetime;
+
+    #[ORM\Column(type: 'string', length: 64)]
+    private string $channel;
+
+    #[ORM\Column(type: 'string', length: 16)]
+    private string $level;
+
+    #[ORM\Column(type: 'text')]
+    private string $message;
+
+    #[ORM\Column(type: 'json', nullable: true)]
+    private ?array $context = null;
+
+    #[ORM\Column(type: 'json', nullable: true)]
+    private ?array $extra = null;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $formatted = null;
+}
