f5a4709254111bbaa1d6e88491ea82c65f6b2043
Ori <ori.rowan@gmail.com>
2026-05-06 17:41:54 +0200

    typo fix

--- src/Service/BreadcrumbsMaker.php
+++ src/Service/BreadcrumbsMaker.php
@@ -10,7 +10,7 @@ use Greendot\EshopBundle\Entity\Project\Product;
 use Greendot\EshopBundle\Repository\Project\CategoryRepository;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Greendot\EshopBundle\Dto\BreadCrumb;
-use Greendot\EshopBundle\Enum\SpecialCategorycEnum;
+use Greendot\EshopBundle\Enum\SpecialCategoryEnum;
 use Greendot\EshopBundle\Repository\Project\CategoryProductRepository;
 use InvalidArgumentException;
 
