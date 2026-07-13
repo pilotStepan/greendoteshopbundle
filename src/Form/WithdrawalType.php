575079cb75176e28b768bb705d34b0091fa702ae
Orion Rowan <ori.rowan@gmail.com>
2026-07-13 15:23:03 +0200

    translatable withdrawl form

--- src/Form/WithdrawalType.php
+++ src/Form/WithdrawalType.php
@@ -14,18 +14,24 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 
 class WithdrawalType extends AbstractType
 {
+
+    public function __construct(
+        private TranslatorInterface $translator
+    ) {}
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, ['label' => 'Vaše jméno a příjmení'])
-            ->add('email', EmailType::class, ['label' => 'Váš e-mail (pro potvrzení)'])
-            ->add('orderNumber', IntegerType::class, ['label' => 'Číslo objednávky'])
-            ->add('goods', TextareaType::class, ['label' => 'Vypište zboží, které z objednávky chcete vrátit'])
-            ->add('bankAccount', TextType::class, ['label' => 'Bankovní účet, kam požaduji převést částku za zboží'])
+            ->add('name', TextType::class, ['label' => $this->translator->trans('Vaše jméno a příjmení')])
+            ->add('email', EmailType::class, ['label' => $this->translator->trans('Váš e-mail (pro zaslání potvrzení)')])
+            ->add('orderNumber', IntegerType::class, ['label' => $this->translator->trans('Číslo objednávky nebo faktury')])
+            ->add('goods', TextareaType::class, ['label' => $this->translator->trans('Vypište zboží, které z objednávky chcete vrátit včetně čísla produktu')])
+            ->add('bankAccount', TextType::class, ['label' => $this->translator->trans('Bankovní účet, kam požaduji převést částku za zboží (pro sk platby uveďte IBAN)')])
             ->add('captcha', Recaptcha3Type::class, [
                 'constraints' => new Recaptcha3(),
                 'locale' => 'cs',
@@ -35,7 +41,7 @@ class WithdrawalType extends AbstractType
                     'id' => 'withdrawal-captcha',
                 ],
             ])
-            ->add('submit', SubmitType::class, ['label' => 'Potvrdit odstoupení od smlouvy'])
+            ->add('submit', SubmitType::class, ['label' => $this->translator->trans('Potvrdit odstoupení od smlouvy')])
         ;
     }
 
